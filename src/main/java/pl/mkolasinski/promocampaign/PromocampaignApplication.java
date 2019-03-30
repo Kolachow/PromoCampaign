7fb7c9ae96a4e21d222ac3c79c4e128a2863d988
Kolachow <kolas3000@gmail.com>
2019-03-30 11:47:32 +0100

    first commit-1

---
+++ src/main/java/pl/mkolasinski/promocampaign/PromocampaignApplication.java
@@ -0,0 +1,13 @@
+package pl.mkolasinski.promocampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocampaignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromocampaignApplication.class, args);
+    }
+
+}
